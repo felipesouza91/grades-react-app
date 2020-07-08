e97c87595ee32f084a78183a8f39201d70982a00
Felipe Souza <felipedb91@gmail.com>
2020-07-07 23:19:06 -0300

    Prepare for deploy

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.BACKEND_URL || 'http://localhost:3333',
   headers: {
     'Content-type': 'application/json',
   },
