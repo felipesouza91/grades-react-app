7c9240e6c37f525e50cab1ed041fff62c49db4f0
Felipe Souza <felipedb91@gmail.com>
2020-07-07 23:32:09 -0300

    Ajustando url do backend

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:3333',
+  baseURL:
+    process.env.BACKEND_URL || 'https://enigmatic-dusk-19196.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
